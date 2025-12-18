31291cbe4479241ece05def61190ac89a4c613ce
gabrielfelipedy <gabriel.felipe.dy@gmail.com>
2025-12-17 23:16:42 -0300

    made subapase work

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
 //import Image from "next/image";
 
 import DataTablePage from "@/components/data-table/DataTablePage";
-import Costs from "@/components/costs/costs";
 import CostsResume from "@/components/costs/costs-resume";
 import EarningResumes from "@/components/earnings/earnings-resume";
 import Balance from "@/components/balance/balance";
+import { createClient } from "@/utils/supabase/server";
 
-export default function Home() {
+export default async function Home() {
+
+  const supabase = await createClient()
+  const result = await supabase.from("category").select("*");
+  console.log(result)
+
   return (
     <section className="mt-4 md:mt-20">
+
       <h1 className="text-[2.2rem] md:text-[4rem] font-bold">Dashboard</h1>
       <p className="text-slate-500 text-xl md:text-2xl">Visão geral das finanças</p>
 
