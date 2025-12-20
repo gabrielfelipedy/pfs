ea921113cde0c18d57f5752ebccb89e2f68a68c2
gabrielfelipedy <gabriel.felipe.dy@gmail.com>
2025-12-20 16:26:49 -0300

    add initial settings page

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+        <h1 className='text-[2rem] font-bold'>Perfil</h1>
+        <h1 className='text-[1.5rem] text-black/60 font-bold'>Alterar username</h1>
+        <h1 className='text-[1.5rem] text-black/60 font-bold'>Alterar senha</h1>
+
+        <h1 className='text-[2rem] font-bold'>Configurações</h1>
+        <h1 className='text-[1.5rem] text-black/60 font-bold'>Editar categorias</h1>
+    </div>
   )
 }
 
--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = ({ linksClassName }: Props) => {
   return (
     <nav className="fixed mt-10 z-50 w-full flex justify-between items-center px-6 md:px-40">
 
-        <div className="items-center py-2 px-4 rounded-lg flex justify-between w-full bg-white/40 dark:bg-black/40 backdrop-blur-lg">
+        <div className="items-center py-2 md:px-4 rounded-lg flex justify-between w-full bg-white/40 dark:bg-black/40 backdrop-blur-lg">
 
       <div className={`${linksClassName} text-sm md:text-md flex gap-6 text-gray-600 dark:text-gray-300`}>
         <Link className="hover:text-black dark:hover:text-white" href={"/"}>Dashborad</Link>
