b3662734bd8ef9ea9a3913c5a5d0be1061869565
gabrielfelipedy <gabriel.felipe.dy@gmail.com>
2025-12-22 14:32:12 -0300

    correct margin on charts

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default async function Home() {
 
       <Resume />
 
-      <div className="mt-8">
+      <div className="mt-8 flex flex-col gap-5">
 
         <Line
         title="Evolução do saldo"
